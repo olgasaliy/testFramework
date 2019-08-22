d0c5ec25bd3c14aa875043adf43fc9cf525d6673
Olha Salii <olha.salii@ctdev.io>
2019-08-22 12:54:27 +0300

    initial commit

---
+++ Example/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Olha Salii on 8/22/19.
+//  Copyright © 2019 Olga Saliy. All rights reserved.
+//
+
+import UIKit
+import TestFramework
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        TestApi().testFunction()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TestFramework/TestApi.swift
@@ -0,0 +1,19 @@
+//
+//  TestApi.swift
+//  TestFramework
+//
+//  Created by Olha Salii on 8/22/19.
+//  Copyright © 2019 Olga Saliy. All rights reserved.
+//
+
+import Foundation
+
+public class TestApi {
+    
+    public init() { }
+    
+    public func testFunction() {
+        print("test function is executed!")
+    }
+    
+}
